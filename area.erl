8a1f4b0777bb490d76bae80a4433a22341d9a1c4
Daniel Henrique Braz de Aquino <dhbaquino@gmail.com>
2012-06-05 20:14:59 -0400

    Gerando raw.erl com passagem de atributos.

--- area.erl
+++ area.erl
@@ -1,2 +1,8 @@
 -module(area).
 -export([new/1, area/1]).
+class_attributes()-> 
+ put({a, R},nil),
+ put({a, J},2.34000000000000013545e-01),
+ put({a, S},teste),
+ put({a, K},4),
+ put({a, Z},nil).
--- src/core.erl
+++ src/core.erl
@@ -8,16 +8,24 @@ transform_uast_to_east(CerlAST, ErlangModuleName) ->
 	Module = "-module(" ++ atom_to_list(ErlangModuleName) ++ ").\n",
 	[_Class, DefinitionList | _] = CerlAST,	
 	OO = [match_definition(Definition)|| Definition <- DefinitionList],
-	lists:flatten(Module ++ OO).
+	Attribute =
+	case CerlAST of
+		[_Class, _DefinitionList, 
+			{'class attributes.', AttributeList, '.'}, _Methods] ->
+			transform_attribute_list(AttributeList);		
+		_ -> 
+			"\n"
+	end,	
+	lists:flatten(Module ++ OO ++ Attribute).
 
 %%------------------------------------------------------------------------------
 %% Pattern Match de Export
 match_definition({export, ExportList}) ->
 	transform_export_list(ExportList).
 
+
 %%------------------------------------------------------------------------------
 %% Transforma lista de export
-
 transform_export_list([{Name, Arity} | Rest]) ->
 	"-export([" ++ atom_to_list(Name) ++ 
 	"/" ++ 
@@ -32,3 +40,30 @@ transform_export_rest([{Name, Arity} | Rest]) ->
 	transform_export_rest(Rest);
 transform_export_rest([]) ->
 	"]).\n".
+
+%%------------------------------------------------------------------------------
+%% Transforma lista de atributos
+transform_attribute_list([{Name, Value}| Rest]) -> 
+	"class_attributes()-> \n put({a, " ++ 
+	atom_to_list(Name) ++ "}," ++ 
+	convert_value(Value) ++
+	")" ++ 
+	transform_attribute_rest(Rest).
+
+transform_attribute_rest([{Name, Value} | Rest]) ->
+	",\n put({a, " ++ 
+	atom_to_list(Name) ++ "}," ++ 
+	convert_value(Value) ++
+	")" ++ 
+	transform_attribute_rest(Rest); 
+transform_attribute_rest([]) ->
+	".\n".
+
+convert_value(Value) when is_integer(Value) -> integer_to_list(Value);
+convert_value(Value) when is_float(Value) -> float_to_list(Value);
+convert_value(Value) when is_atom(Value) -> 
+	case string:to_lower(atom_to_list(Value)) of
+		"null" 	-> "nil";
+		_	-> atom_to_list(Value)
+	end;
+convert_value(Value) when is_list(Value) -> Value.
