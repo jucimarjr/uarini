0869d48470db4687f7056c20e7571a7c180170af
Daniel Henrique Braz de Aquino <dhbaquino@gmail.com>
2012-08-02 16:36:03 -0400

    Modelo de Pingping em .erl

--- src_cerl/PingPing/PingPing - ERL/main.erl
+++ src_cerl/PingPing/PingPing - ERL/main.erl
@@ -1,8 +1,12 @@
--method(main).
--export([new/2, pingping/2]).
+-module(main).
+-compile(export_all).
 
+start_class() ->
+	ooe:def_class(?MODULE),
+	ooe:def_method(?MODULE, {run, 2}, fun(A, B) -> run(A, B) end).
+	
 run(R, D) ->
-	P1 = def_constructor({Ping, [pingping, [D]]}),
-	P2 = ooe:def_constructor({Ping, [pingping, [D]]}),
+	P1 = ooe:constructor(ping, {new, 1}, [D]),
+	P2 = ooe:constructor(ping, {new, 1}, [D]),
 	P1 ! {init, P2, R},
-    P2 ! {init, P1, R}.
+	P2 ! {init, P1, R}.
