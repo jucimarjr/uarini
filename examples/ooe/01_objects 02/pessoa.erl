2fdedda307afde35f325efb55978d6f9d7717b5c
Daniel Henrique Braz de Aquino <dhbaquino@gmail.com>
2012-10-10 00:43:24 -0400

    Subindo segundo exemplos de objetos

---
+++ examples/ooe/01_objects 02/pessoa.erl
@@ -0,0 +1,31 @@
+-module(pessoa, [Nome, Nascimento]).
+-export([constructor/0]).
+-export([gastar/2, receber/2, get_dinheiro/1]).
+
+constructor() ->
+	Key = key(),
+	put({pessoa, nome, Key}, Nome),
+	put({pessoa, nascimento, Key}, Nascimento),
+	put({pessoa, dinheiroNaCarteira, Key}, 0),
+	Key.
+
+gastar(Valor, Key) ->
+	put({pessoa, dinheiroNaCarteira, Key}, 
+			get({pessoa, dinheiroNaCarteira, Key}) - Valor).	
+
+receber(Valor, Key) ->
+	put({pessoa, dinheiroNaCarteira, Key}, 
+			get({pessoa, dinheiroNaCarteira, Key}) + Valor).
+
+get_dinheiro(Key) ->
+	get({pessoa, dinheiroNaCarteira, Key}).
+
+key() ->
+	case get({pessoa, key}) of
+		undefined ->
+			put({pessoa, key}, 0),
+			0;
+		Key ->
+			put({pessoa, key}, Key + 1),
+			Key + 1
+	end.
