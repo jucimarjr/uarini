9360856a1b13df39616056bf95a6e599c002201d
Emiliano Firmino <elmiliox@gmail.com>
2012-11-28 20:33:53 -0400

    refatorado uarini_parse_tests e adicionado teste de declaracao de variavel

--- scan_parse/uarini_parse_tests.erl
+++ scan_parse/uarini_parse_tests.erl
@@ -8,35 +8,45 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
+-define(OK(RV1, RV2), {ok, RV1, RV2}).
+-define(OK(ReturnValue), {ok, ReturnValue}).
+
 get_tokens(Source) ->
-    {ok, Tokens, _Lines} = uarini_scan:string(Source),
+    ?OK(Tokens, _Lines) = uarini_scan:string(Source),
     Tokens.
 
 uarini_tag_test() ->
-    Exp1 = {ok, {attribute,1,class,      name}},
-    Exp2 = {ok, {attribute,1,interface,  name}},
-    Exp3 = {ok, {attribute,1,extends,    name}},
-    Exp4 = {ok, {attribute,1,implements, [c1, c2, c3]}},
-    Exp5 = {ok, {attribute,1,constructor, [{new,0},{new,1}]}},
+    Exp1 = {attribute, 1, class, name},
+    Exp2 = {attribute, 1, extends, name},
+    Exp3 = {attribute, 1, interface, name},
+    Exp4 = {attribute, 1, implements, [c1, c2, c3]},
+    Exp5 = {attribute, 1, constructor, [{new,0},{new,1}]},
     
     Form1 = uarini_parse:parse(get_tokens("-class(name).")),
-    Form2 = uarini_parse:parse(get_tokens("-interface(name).")),
-    Form3 = uarini_parse:parse(get_tokens("-extends(name).")),
+    Form2 = uarini_parse:parse(get_tokens("-extends(name).")),
+    Form3 = uarini_parse:parse(get_tokens("-interface(name).")),
     Form4 = uarini_parse:parse(get_tokens("-implements([c1,c2,c3]).")),
     Form5 = uarini_parse:parse(get_tokens("-constructor([new/0, new/1]).")),
 
-    [?assertEqual(Exp1, Form1),
-     ?assertEqual(Exp2, Form2),
-     ?assertEqual(Exp3, Form3),
-     ?assertEqual(Exp4, Form4),
-     ?assertEqual(Exp5, Form5)].
+    [?assertEqual(?OK(Exp1), Form1),
+     ?assertEqual(?OK(Exp2), Form2),
+     ?assertEqual(?OK(Exp3), Form3),
+     ?assertEqual(?OK(Exp4), Form4),
+     ?assertEqual(?OK(Exp5), Form5)].
 
 uarini_markup_test() ->
-    Exp1 = {ok, {class_attributes, 1}},
-    Exp2 = {ok, {class_methods,    1}},
+    Exp1 = {class_attributes, 1},
+    Exp2 = {class_methods, 1},
     
     Form1 = uarini_parse:parse(get_tokens("class_attributes.")),
     Form2 = uarini_parse:parse(get_tokens("class_methods.")),
 
-    [?assertEqual(Exp1, Form1),
-     ?assertEqual(Exp2, Form2)].
+    [?assertEqual(?OK(Exp1), Form1),
+     ?assertEqual(?OK(Exp2), Form2)].
+
+uarini_var_test() ->
+    Exp1 = [{[],[],{atom,1,'NoType'}, {var,1,'MyVar'}}],
+
+    Form1 = uarini_parse:parse(get_tokens("MyVar.")),
+
+    [?assertEqual(?OK(Exp1), Form1)].
