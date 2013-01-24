51b5347c636f65248f5ea4e779bfa84ca865485c
Jucimar Jr <jucimar.jr@gmail.com>
2013-01-24 10:50:07 -0400

    continuando os mpi...

--- src/aleppo_parser.erl
+++ src/aleppo_parser.erl
@@ -1,7 +1,7 @@
 -module(aleppo_parser).
 -export([parse/1, parse_and_scan/1, format_error/1]).
 
--file("c:/PROGRA~2/erl5.9/lib/parsetools-2.0.7/include/yeccpre.hrl", 0).
+-file("/usr/local/lib/erlang/lib/parsetools-2.0.7/include/yeccpre.hrl", 0).
 %%
 %% %CopyrightBegin%
 %%
