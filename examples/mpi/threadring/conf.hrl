2d2c3aa361a6608a2504f7a485613262fa2bd261
Rodrigo Bernardino <rbbernardino@gmail.com>
2013-01-27 16:55:38 -0400

    aletrando config do threadring

--- examples/mpi/threadring/conf.hrl
+++ examples/mpi/threadring/conf.hrl
@@ -2,4 +2,4 @@
 -define(ERR_REPORT(Msg, Reason), io:format("%%%%%%%%%%%%%% ERRO %%%%%%%%%%%%%%\n" ++
 				     "Msg: ~w\n" ++
 				     "Reason: ~w\n\n", [Msg, Reason])).
--define(OUT_PATH, "../../docs/erlang/out_erl_threadring.txt").
+-define(OUT_PATH, "./out_erl_threadring.txt").
